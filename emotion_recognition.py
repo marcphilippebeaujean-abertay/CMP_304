020a4d83e00736fb43610cfc7794182f5ead8035
Marc Philippe Beaujean <marcphilippebeaujean@Marcs-MacBook-Pro.local>
2018-04-01 20:43:29 +0200

    created basic SURF feature application

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 # # # MAIN APPLICATION
 img_grayscale = cv2.imread('102_su_000_0744.jpg', 0);
 # Create sift features
-sift = cv2.xfeatures2d.SIFT_create();
+surf = cv2.xfeatures2d.SURF_create();
 # Detect the key points in the image
-key_points = sift.detect(img_grayscale);
+key_points = surf.detect(img_grayscale);
 # Create array of zeros with the same shape and type as a given array
 image_key_points = np.zeros_like(img_grayscale);
 # Draw key points on the image
@@ -18,5 +18,5 @@ image_key_points = cv2.drawKeypoints(img_grayscale, key_points, image_key_points
 # Plot Image
 plt.imshow(image_key_points);
 # Create feature discriptors
-key_points, des = sift.compute(img_grayscale, key_points);
-print(des.shape);
+key_points, feature_descriptors = surf.compute(img_grayscale, key_points);
+print(feature_descriptors.shape);
