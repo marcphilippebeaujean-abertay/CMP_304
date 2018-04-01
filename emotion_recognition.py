61446e7ec9b269554dc6a8d7cd6d1e24f020bbee
Marc Philippe Beaujean <marcphilippebeaujean@Marcs-MacBook-Pro.local>
2018-04-01 20:41:10 +0200

    created SIFT features and output them to the console

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -4,19 +4,19 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# # # INITIALISE VARIABLES # # #
-
-
-# # # CREATING A k-NEAREST NEIGHBOUR CLASSIFIERs
-
-# Generate a random numpy seed
-
-def SIFT_FeatureExtraction(grayscale_image):
-    
-
-    return grayscale_image;
 
 # # # MAIN APPLICATION
 img_grayscale = cv2.imread('102_su_000_0744.jpg', 0);
-corners = cv2.cornerHarris(img_grayscale, 3, 15, 0.04)
-plt.imshow(corners, cmap='gray');
+# Create sift features
+sift = cv2.xfeatures2d.SIFT_create();
+# Detect the key points in the image
+key_points = sift.detect(img_grayscale);
+# Create array of zeros with the same shape and type as a given array
+image_key_points = np.zeros_like(img_grayscale);
+# Draw key points on the image
+image_key_points = cv2.drawKeypoints(img_grayscale, key_points, image_key_points, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# Plot Image
+plt.imshow(image_key_points);
+# Create feature discriptors
+key_points, des = sift.compute(img_grayscale, key_points);
+print(des.shape);
